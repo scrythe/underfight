89e88a80a89f3c1dfdcf6c87cf8d339c388587da
Magomed <scrythepvp@gmail.com>
2022-05-07 18:50:55 +0200

    feat: basic phaser setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('hello');
+import Phaser from 'phaser';
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: {
+    preload,
+    create,
+    update,
+  },
+};
+
+function preload() {}
+function create() {}
+function update() {}
+
+const game = new Phaser.Game(config);
